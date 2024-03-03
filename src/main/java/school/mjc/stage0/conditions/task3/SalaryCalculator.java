ed31f7d493dc2607756a20ee2146a54063b94b51
mymindinsane <arturguletskiy21@gmail.com>
2024-03-03 11:51:24 +0300

    fix SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if (salary <= 10000){
+        if (salary <= 10000 && salary > 0){
             System.out.println(salary * 0.85);
         } else if (salary > 10000 && salary <= 20000){
             System.out.println(salary * 0.82);
