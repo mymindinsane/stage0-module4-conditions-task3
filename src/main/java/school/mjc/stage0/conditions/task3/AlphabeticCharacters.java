bdb26203a58535bb3e4f317ea619d5febc7d57ae
mymindinsane <arturguletskiy21@gmail.com>
2024-03-03 11:44:50 +0300

    done AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -5,7 +5,7 @@ public class AlphabeticCharacters {
         if (character == 65 || character == 97 || character == 69 || character == 101 || character == 73 || character == 105
         || character == 79 || character == 111 || character == 85 || character == 117){
             System.out.println("Vowel");
-        } if (character < 65 || (character > 90 && character < 97) || character > 122) {
+        } else if (character < 65 || (character > 90 && character < 97) || character > 122) {
             System.out.println("wrong alphabet!");
         } else {
             System.out.println("Consonant");
